c7ee1a95381e9ed05f86037a83704d5e1e6e3c44
stgAkssel <aissam.oukraou@gmail.com>
2023-04-30 15:02:05 +0200

    Documenting the two last classes Personnage and position

--- Projet/src/src/src/Personnage.h
+++ Projet/src/src/src/Personnage.h
@@ -5,7 +5,11 @@
 #include <cstring>
 #include "Dimension.h"
 #include "Position.h"
-
+/**
+ * @class Personnage[Personnage.hpp]
+ * @brief Regroupe les caractéristiques de notre joueur.
+ * @details 'Personnage' précise toutes les caractéristiques et fonctions nécessaire à notre joueur dans le 'Jeu'.
+ */ 
 class Personnage
 {
 private:
@@ -26,31 +30,81 @@ private:
     unsigned int Perso_Saute;
 
 public:
+    /// @brief Constructeur de la class 'Personnage'
     Personnage();
     Personnage(std::string Pseudo, Position Pos, Dimension Dim, unsigned int nbPixelParCarreau, unsigned int Saut);
+    
+    ///@brief Destructeur de la class 'Personnage'
     ~Personnage();
 
+    /// @brief Avoir le Pseudo de personnage.
+    /// @return Une chaine de caractères représentant le pseudo du joueur.
     std::string getPseudo() const;
+
+    /// @brief Avoir la position de notre joueur.
+    /// @return Position de joueur sur la carte. 
     Position getPosition() const;
+
+    /// @brief Avoir la dimension de notre personnage. 
+    /// @return Dimension de personnage.
     Dimension getDimension() const;
     // std::string getImage() const;
+
+    /// @brief Verifie la vie de notre joueur.
+    /// @return 'True' si le personnage est vivant, 'false' sinon.
     bool getVie() const;
+
+    /// @brief Le nombre de pixel par Carreau pour avoir les dimensions. 
+    /// @return Nombre de pixel par carreau.
     unsigned int getNbPixelParCarreau() const;
+
+    /// @brief Avoir le niveau de saut de personnage sur la carte.
+    /// @return Saut de joueur.
     unsigned int getSaut() const;
 
+    /// @brief Modifie le psudo de personnage.
+    /// @param[in] pseudo le nouveau pseudo à mettre. 
     void setPseudo(std::string pseudo);
+
+    /// @brief Nouvelle position de notre joueur.
+    /// @param[in] Pos Nouvelle position de notre personnage sur la carte. 
     void setPosition(Position Pos);
+
+    /// @brief Changer l'abscisse pour le déplacement horizental du personnage sur la carte. 
+    /// @param[in] abscisse Nouveau abscisse.
     void setAbscisse(unsigned int abscisse);
+
+    /// @brief Changer l'ordonné pour le déplacement vertical du personnage sur la carte.
+    /// @param[in] ordonnée Nouveau ordonnée. 
     void setOrdonnee(unsigned int ordonnée);
+
+    /// @brief Modifier la dimension.
+    /// @param[in] Dim La nouvelle dimension à remplacer avec.  
     void setDimension(Dimension Dim);
     // void setImage(std::string image);
+
+    /// @brief Modifier la vie de notre personnage.
+    /// @param[in] vie La nouvelle 'vie' à mettre. 
     void setVie(bool vie);
+
+    /// @brief Changement du nombre de pixel par chaque carreau sur la carte.
+    /// @param[in] nbPixelParCarreau Le nouveau nombre de pixels à mettre par carreau.
     void setNbPixelParCarreau(unsigned int nbPixelParCarreau);
+
+    /// @brief Modifie le niveau de saut du personnage sur la carte.
+    /// @param[in] saut Le nouveau saut à adopter.
     void setSaut(unsigned int saut);
 
+    /// @brief Changement du direction du personnage vers la gauche. 
     void perso_versGauche();
+
+    /// @brief Changement du direction du personnage vers la droite.
     void perso_versDroite();
+
+    /// @brief Faire sauter le personnage.  
     void perso_sauter();
+
+    /// @brief Faire tomber le personnage. 
     void perso_tomber();
 
 };
--- Projet/src/src/src/Position.h
+++ Projet/src/src/src/Position.h
@@ -1,35 +1,60 @@
 #ifndef _POSITION_H
 #define _POSITION_H
-
+/**
+ * @class Position[Position.hpp]
+ * @brief Gère le positionnement de nos objets du jeu.
+ * @details Regroupe les fonctions nécessaires au traitement du positionement lié au jeu. 
+*/
 class Position
 {
 private:
-
+    /// @brief Le système de positionnement avec des abs X et ord Y.
     int Pos_abscisse,Pos_ordonnee;
 
 
 
 public:
+    /// @brief Constructeur de la class.
     Position(/* args */);
-
     Position(unsigned int abscisse, unsigned int ordonnee);
 
+    ///@brief Destructeur de la class.
     ~Position();
 
-//Accesseurs
+    /// @brief Récupèrer l'abscisse 'x' d'objet.
+    /// @return Abscisse 'x'.
     unsigned int getAbscisse() const;
+
+    /// @brief Récupèrer l'ordonnée 'Y' d'objet.
+    /// @return Ordonnée 'Y'
     unsigned int getOrdonnee() const;
+
+    /// @brief Avoir la distance à l'objet à partir d'une position.
+    /// @param[in] Pos La position à partir de laquelle on calcule la distance. 
+    /// @return Distance.
     unsigned int getDistance(const Position &Pos) const;
+
+    /// @brief Avoir la disctance suivant l'axe des abscisses 'x'.
+    /// @param [in] Point2 
+    /// @return Distance suivant 'x'.
     unsigned int getDistanceAbs(const Position &Point2) const;
 
-    // Mutateurs
+    /// @brief Modifier la composante des abscisses 'x'.
+    /// @param abscisse Nouveau abscisse 'x'.
     void setAbscisse(unsigned int abscisse);
+
+    /// @brief Modifier la composante des ordonnées 'Y'.
+    /// @param ordonnee Nouvel ordonnée 'Y'.
     void setOrdonnee(unsigned int ordonnee);
 
-//Surcharge d'opérateurs
-
+   /// @brief Surcharger l'operateur d'affectation pour les positions.
+   /// @param [in] positon Position à affecter. 
+   /// @return Position.
    Position& operator = (const Position &positon);
 
+   /// @brief Surcharge d'operateur d'égalité pour pour la comparison des positions.
+   /// @param[in] p la position comparée. 
+   /// @return 'True' si les deux positions sont égales, 'false' sinon.
    bool operator ==(const Position &p);
 };
 
